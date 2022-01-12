86b5dbdb8c63871917eb47a35dbe94a50b12ac61
little-pinecone <marta.szymek.mail@gmail.com>
2022-01-12 11:52:10 +0100

    feat: initial commit

---
+++ src/main/java/in/keepgrowing/keycloakspringboot/KeycloakSpringBootApplication.java
@@ -0,0 +1,13 @@
+package in.keepgrowing.keycloakspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/keepgrowing/keycloakspringboot/KeycloakSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package in.keepgrowing.keycloakspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

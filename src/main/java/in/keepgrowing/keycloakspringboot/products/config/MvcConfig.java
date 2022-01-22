18cc44dd9323111f6e27464d83bd65d79710eb7a
little-pinecone <marta.szymek.mail@gmail.com>
2022-01-22 17:24:50 +0100

    feat: configure common api prefix for all paths used in controllers

---
+++ src/main/java/in/keepgrowing/keycloakspringboot/products/config/MvcConfig.java
@@ -0,0 +1,29 @@
+package in.keepgrowing.keycloakspringboot.products.config;
+
+import in.keepgrowing.keycloakspringboot.KeycloakSpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.function.Predicate;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public static final String API_PREFIX = "api";
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix(API_PREFIX, createPredicateForControllers());
+    }
+
+    private Predicate<Class<?>> createPredicateForControllers() {
+        String packageName = KeycloakSpringBootApplication.class.getPackageName();
+
+        return HandlerTypePredicate
+                .forBasePackage(packageName)
+                .and(HandlerTypePredicate.forAnnotation(RestController.class));
+    }
+}

26d361f8aad544d2c998f86795119b8241f77f97
little-pinecone <marta.szymek.mail@gmail.com>
2022-01-22 17:13:37 +0100

    feat: add API facade for products

---
+++ src/main/java/in/keepgrowing/keycloakspringboot/products/adapters/driving/api/http/services/ProductHttpApiFacade.java
@@ -0,0 +1,24 @@
+package in.keepgrowing.keycloakspringboot.products.adapters.driving.api.http.services;
+
+import in.keepgrowing.keycloakspringboot.products.adapters.driving.api.http.model.responses.ProductResponse;
+import in.keepgrowing.keycloakspringboot.products.domain.model.Product;
+import in.keepgrowing.keycloakspringboot.products.domain.ports.persistence.ProductRepository;
+
+import java.util.List;
+
+public class ProductHttpApiFacade {
+
+    private final ProductRepository productRepository;
+
+    public ProductHttpApiFacade(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<ProductResponse> findAll() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream()
+                .map(ProductResponse::from)
+                .toList();
+    }
+}
---
+++ src/test/java/in/keepgrowing/keycloakspringboot/products/adapters/driving/api/http/services/ProductHttpApiFacadeTest.java
@@ -0,0 +1,46 @@
+package in.keepgrowing.keycloakspringboot.products.adapters.driving.api.http.services;
+
+import in.keepgrowing.keycloakspringboot.products.adapters.driving.api.http.model.responses.ProductResponse;
+import in.keepgrowing.keycloakspringboot.products.domain.model.Product;
+import in.keepgrowing.keycloakspringboot.products.domain.model.TestProductProvider;
+import in.keepgrowing.keycloakspringboot.products.domain.ports.persistence.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProductHttpApiFacadeTest {
+
+    private ProductHttpApiFacade apiFacade;
+    private TestProductProvider productProvider;
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @BeforeEach
+    void setUp() {
+        apiFacade = new ProductHttpApiFacade(productRepository);
+        productProvider = new TestProductProvider();
+    }
+
+    @Test
+    void shouldReturnAllProducts() {
+        Product product = productProvider.full();
+        var products = List.of(product);
+        var expectedElement = ProductResponse.from(product);
+
+        when(productRepository.findAll())
+                .thenReturn(products);
+
+        List<ProductResponse> actual = apiFacade.findAll();
+
+        assertTrue(actual.contains(expectedElement));
+    }
+}

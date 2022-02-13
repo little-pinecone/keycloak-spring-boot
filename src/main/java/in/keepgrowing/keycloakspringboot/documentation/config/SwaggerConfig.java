8287f9be8379a17c8333f87ea662141269b42d97
little-pinecone <marta.szymek.mail@gmail.com>
2022-02-13 19:08:04 +0100

    refactor: relocate Swagger Components creation
    * specify components config alongside other Swagger options
    * fix method visibility
    * rename variables and methods

--- src/main/java/in/keepgrowing/keycloakspringboot/documentation/config/SwaggerConfig.java
+++ src/main/java/in/keepgrowing/keycloakspringboot/documentation/config/SwaggerConfig.java
@@ -20,16 +20,15 @@ public class SwaggerConfig {
     private static final String PROTOCOL_URL_FORMAT = "%s/realms/%s/protocol/openid-connect";
 
     @Bean
-    OpenAPI customOpenApi(SwaggerProperties swaggerProperties, KeycloakProperties keycloakProperties) {
-        var openApi = new OpenAPI()
-                .info(getInfo(swaggerProperties));
-
-        configureSecurity(openApi, keycloakProperties);
-
-        return openApi;
+    public OpenAPI customOpenApi(SwaggerProperties swaggerProperties, KeycloakProperties keycloakProperties) {
+        return new OpenAPI()
+                .info(getInfo(swaggerProperties))
+                .components(new Components()
+                        .addSecuritySchemes(OAUTH_SCHEME_NAME, createOAuthScheme(keycloakProperties)))
+                .addSecurityItem(new SecurityRequirement().addList(OAUTH_SCHEME_NAME));
     }
 
-    public Info getInfo(SwaggerProperties swaggerProperties) {
+    private Info getInfo(SwaggerProperties swaggerProperties) {
         return new Info()
                 .title(swaggerProperties.projectTitle())
                 .description(swaggerProperties.projectDescription())
@@ -43,36 +42,22 @@ public class SwaggerConfig {
                 .url("https://unlicense.org/");
     }
 
-    private void configureSecurity(OpenAPI openApi, KeycloakProperties properties) {
-        Components components = createComponentsWithSecurityScheme(properties);
-        openApi
-                .components(components)
-                .addSecurityItem(new SecurityRequirement().addList(OAUTH_SCHEME_NAME));
-    }
-
-    private Components createComponentsWithSecurityScheme(KeycloakProperties properties) {
-        SecurityScheme oAuthScheme = createOAuthScheme(properties);
-
-        return new Components()
-                .addSecuritySchemes(OAUTH_SCHEME_NAME, oAuthScheme);
-    }
-
     private SecurityScheme createOAuthScheme(KeycloakProperties properties) {
-        OAuthFlows flows = createAuthFlows(properties);
+        OAuthFlows flows = createOAuthFlows(properties);
 
         return new SecurityScheme()
                 .type(SecurityScheme.Type.OAUTH2)
                 .flows(flows);
     }
 
-    private OAuthFlows createAuthFlows(KeycloakProperties properties) {
-        OAuthFlow authCodeFlow = createAuthCodeFlow(properties);
+    private OAuthFlows createOAuthFlows(KeycloakProperties properties) {
+        OAuthFlow flow = createAuthorizationCodeFlow(properties);
 
         return new OAuthFlows()
-                .authorizationCode(authCodeFlow);
+                .authorizationCode(flow);
     }
 
-    private OAuthFlow createAuthCodeFlow(KeycloakProperties properties) {
+    private OAuthFlow createAuthorizationCodeFlow(KeycloakProperties properties) {
         var protocolUrl = String.format(PROTOCOL_URL_FORMAT, properties.authServerUrl(), properties.realm());
 
         return new OAuthFlow()

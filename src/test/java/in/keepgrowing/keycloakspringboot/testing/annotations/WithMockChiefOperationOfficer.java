c8e1f970e719bb75e3930a58f684a708b5340040
little-pinecone <marta.szymek.mail@gmail.com>
2022-03-05 19:04:29 +0100

    feat: specify target for WithMockChiefOperationOfficer annotation

--- src/test/java/in/keepgrowing/keycloakspringboot/testing/annotations/WithMockChiefOperationOfficer.java
+++ src/test/java/in/keepgrowing/keycloakspringboot/testing/annotations/WithMockChiefOperationOfficer.java
@@ -2,10 +2,13 @@ package in.keepgrowing.keycloakspringboot.testing.annotations;
 
 import org.springframework.security.test.context.support.WithMockUser;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@WithMockUser(roles="CHIEF-OPERATING-OFFICER")
+@WithMockUser(roles = "CHIEF-OPERATING-OFFICER")
 public @interface WithMockChiefOperationOfficer {
 }
